4296237b7e3e469a61f7d29fd94c7add39848818
Mayco Barale <maycobarale@gmail.com>
2023-07-15 10:54:36 -0300

    Add race type

--- types.d.ts
+++ types.d.ts
@@ -1,9 +1,13 @@
+/* eslint-disable no-unused-vars */
 type Link = {
   title: string
   link: string
 }
 
-type User = {
+type Race = {
+  date: string
   name: string
+  discountCode: string
+  imageUrl: string
   url: string
 }
