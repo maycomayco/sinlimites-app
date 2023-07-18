ca6c63e73ad993d13a9a7affc631ebef0ff30beb
Mayco Barale <maycobarale@gmail.com>
2023-07-18 10:48:30 -0300

    Add Google font support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,20 @@
 import './globals.css'
+import { Hepta_Slab } from 'next/font/google'
 
 type Props = {
   children: React.ReactNode
 }
 
-export default function RootLayout({
-  children,
-}: Props) {
+const hepta = Hepta_Slab({ subsets: ['latin'] })
+
+export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={`${hepta.className} container bg-neutral-600 text-white min-h-screen px-4`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
