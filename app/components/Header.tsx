8e3ee3344f5c9e551adca5de9ffd8ee52353c186
Mayco Barale <maycobarale@gmail.com>
2023-07-21 11:07:46 -0300

    Add link to logo image

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,19 +1,22 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Header() {
   return (
-    <div className="p-6 mx-auto flex items-center justify-center gap-4">
+    <div className="mx-auto flex items-center justify-center gap-4 p-6">
       <div className="shrink-0">
-        <Image
-          alt="Sin Limites Training Logo"
-          height={90}
-          src="https://d1fdloi71mui9q.cloudfront.net/nTwbCUzTymqtqfWpMMds_cvWMkSuWko58WcFM"
-          width={90}
-        />
+        <Link href="/">
+          <Image
+            alt="Sin Limites Training Logo"
+            height={90}
+            src="https://d1fdloi71mui9q.cloudfront.net/nTwbCUzTymqtqfWpMMds_cvWMkSuWko58WcFM"
+            width={90}
+          />
+        </Link>
       </div>
       <div>
-        <h1 className="text-4xl font-medium font-medium">Sin Limites</h1>
-        <small className="">Donde nada ni nadie esta chequeado</small>
+        <h1 className="text-3xl font-medium">Sin Limites</h1>
+        <small>Donde nada ni nadie esta chequeado</small>
       </div>
     </div>
   )
