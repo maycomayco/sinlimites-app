71c7443bfed5e05d58a2660a6615b8e3ce43f491
Mayco Barale <maycobarale@gmail.com>
2023-08-02 09:09:47 -0300

    Add support for external image sources

--- next.config.js
+++ next.config.js
@@ -6,6 +6,30 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'd1fdloi71mui9q.cloudfront.net',
       },
+      {
+        protocol: 'https',
+        hostname: 'nonoultratrail.com.ar',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ecotrail.run',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.utacchultratrail.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tyr.com.ar',
+      },
+      {
+        protocol: 'https',
+        hostname: 'xtremerace.com.ar',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i0.wp.com',
+      },
     ],
   },
 }
