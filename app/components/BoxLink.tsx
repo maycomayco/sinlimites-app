7cc0db7bb213a5135d4f1780c0e958fc8745d6c1
Mayco Barale <maycobarale@gmail.com>
2023-08-02 09:10:55 -0300

    Add path for BoxLink component

--- app/components/BoxLink.tsx
+++ app/components/BoxLink.tsx
@@ -5,15 +5,17 @@ import { toKebabCase } from '@/lib/utils'
 
 type Props = {
   content: string
+  path?: string
 }
 
-export default function BoxLink({ content }: Props) {
+export default function BoxLink({ content, path }: Props) {
+  const href = path
+    ? `/${path}/${toKebabCase(content)}`
+    : `/${toKebabCase(content)}`
+
   return (
     <li className="rounded-2xl border-2 border-solid border-black bg-white px-10 py-5 text-center text-black">
-      <Link
-        className="flex items-center justify-between text-xl"
-        href={`/${toKebabCase(content)}`}
-      >
+      <Link className="flex items-center justify-between text-xl" href={href}>
         <p>{content}</p>
         <FaAngleRight />
       </Link>
